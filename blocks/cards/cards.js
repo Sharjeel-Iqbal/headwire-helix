f3219452c0b18c30d66ded5581f2880f0c7e92f8
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2022-06-13 10:55:01 -0700

    Added styling and functionality for cards for both dark variation and original. Added footer styles

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -13,19 +13,24 @@ export default function decorate(block) {
   [...cards.children].forEach((card) => {
     card.querySelector('picture').closest('p').className = 'card-image-container';
     const contentBlock = document.createElement('div');
-
+    card.querySelector('h2').className = 'card-heading';
     // Store title description inside a div separating itself from the picture
     [...card.children].forEach((content) => {
       if (content.className !== 'card-image-container' && content.hasChildNodes()) {
         contentBlock.append(content);
       }
-
       // Removes unneeded nodes and paragraph tags.
       if (!content.hasChildNodes()) {
         content.remove();
       }
     });
     contentBlock.className = 'card-content';
+    const contentDescription = contentBlock.querySelectorAll('p');
+    if (contentDescription.length > 0) {
+      for (let i = 0; i < contentDescription.length; i += 1) {
+        contentDescription[i].className = 'card-description';
+      }
+    }
     card.append(contentBlock);
     card.className = 'card';
   });
